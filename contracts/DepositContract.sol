e5ae6cc89cfaae9e9ae8581ee35b643fd2db6917
xPainite <109281998+polarydev@users.noreply.github.com>
2022-07-26 23:02:34 +0700

    audit fix issues

--- contracts/DepositContract.sol
+++ contracts/DepositContract.sol
@@ -1,6 +1,6 @@
 // SPDX-License-Identifier: MIT
 
-pragma solidity ^0.8.13;
+pragma solidity 0.8.15;
 
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
@@ -9,7 +9,7 @@ import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/utils/cryptography/draft-EIP712.sol";
 import "@openzeppelin/contracts/utils/cryptography/ECDSA.sol";
 
-contract DepositContract is AccessControl, Ownable, EIP712 {
+contract DepositContract is AccessControl, EIP712 {
     using SafeERC20 for IERC20;
     
     event DepositedToken(address indexed tokenAddress, address indexed sender, uint256 quantity, uint256 amount, uint256 nonce);
@@ -18,26 +18,27 @@ contract DepositContract is AccessControl, Ownable, EIP712 {
     bytes32 public constant ADMIN_ROLE = keccak256("ADMIN_ROLE");
     bytes32 public constant DEPOSIT_ROLE = keccak256("DEPOSIT_ROLE");
     
-    bytes32 constant public DEPOSIT_TYPEHASH = keccak256("DepositToken(address account,uint256 quantity,uint256 amount,uint256 deadline)");
+    bytes32 constant public DEPOSIT_TYPEHASH = keccak256("DepositToken(address account,uint256 quantity,uint256 amount,uint256 deadline,uint256 nonce)");
 
     address private _acceptToken;
     uint256 private _nonce;
 
     constructor(address acceptToken) EIP712("DepositContract", "1.0.0") {
         _acceptToken = acceptToken;
+        _setupRole(DEFAULT_ADMIN_ROLE, _msgSender());
     }
 
     /**
     @dev Setup multisig admin role
      */
-    function setupAdminRole(address admin) external onlyOwner {
+    function setupAdminRole(address admin) external onlyRole(DEFAULT_ADMIN_ROLE) {
         _grantRole(ADMIN_ROLE, admin);
     }
 
     /**
     @dev Setup deposit role
      */
-    function setupDepositRole(address account) external onlyOwner {
+    function setupDepositRole(address account) external onlyRole(DEFAULT_ADMIN_ROLE) {
         _grantRole(DEPOSIT_ROLE, account);
     }
     
@@ -52,15 +53,16 @@ contract DepositContract is AccessControl, Ownable, EIP712 {
     function depositToken(uint256 quantity, uint256 amount, uint256 deadline, bytes calldata signature) external {
         require(msg.sender == tx.origin, "Contract address is not allowed");
         require(block.timestamp <= deadline, "Invalid expiration in deposit");
-        require(_verify(_hash(msg.sender, quantity, amount, deadline), signature), "Invalid signature");
+        uint256 currentValidNonce = _nonce;
+        require(_verify(_hash(msg.sender, quantity, amount, deadline, currentValidNonce), signature), "Invalid signature");
         unchecked {
             ++_nonce;
         }
         IERC20(_acceptToken).safeTransferFrom(msg.sender, address(this), amount);
-        emit DepositedToken(_acceptToken, msg.sender, quantity, amount, _nonce);
+        emit DepositedToken(_acceptToken, msg.sender, quantity, amount, currentValidNonce);
     }
     
-    function _hash(address account, uint256 quantity, uint256 amount, uint256 deadline)
+    function _hash(address account, uint256 quantity, uint256 amount, uint256 deadline, uint256 nonce)
     internal view returns (bytes32)
     {
         return _hashTypedDataV4(keccak256(abi.encode(
@@ -68,7 +70,8 @@ contract DepositContract is AccessControl, Ownable, EIP712 {
             account,
             quantity,
             amount,
-            deadline
+            deadline,
+            nonce
         )));
     }
 
