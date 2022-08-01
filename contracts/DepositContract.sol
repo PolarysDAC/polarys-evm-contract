bef7862f48195c575372ded7510664f0fd81ca80
xPainite <109281998+polarydev@users.noreply.github.com>
2022-08-01 22:31:20 +0700

    modify depositToken, add merkleProof verify

--- contracts/DepositContract.sol
+++ contracts/DepositContract.sol
@@ -8,6 +8,7 @@ import "@openzeppelin/contracts/access/AccessControl.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/utils/cryptography/draft-EIP712.sol";
 import "@openzeppelin/contracts/utils/cryptography/ECDSA.sol";
+import "@openzeppelin/contracts/utils/cryptography/MerkleProof.sol";
 
 contract DepositContract is AccessControl, EIP712 {
     using SafeERC20 for IERC20;
@@ -15,18 +16,23 @@ contract DepositContract is AccessControl, EIP712 {
     event DepositedToken(address indexed tokenAddress, address indexed sender, uint256 quantity, uint256 amount, uint256 nonce);
     event WithdrawedToken(address indexed tokenAddress, address indexed recipient, uint256 amount);
     
+    error InvalidPrivateSaleAddress(address account);
+
     bytes32 public constant ADMIN_ROLE = keccak256("ADMIN_ROLE");
     bytes32 public constant DEPOSIT_ROLE = keccak256("DEPOSIT_ROLE");
     
-    bytes32 constant public DEPOSIT_TYPEHASH = keccak256("DepositToken(address account,uint256 quantity,uint256 amount,uint256 deadline,uint256 nonce)");
+    bytes32 constant public DEPOSIT_TYPEHASH = keccak256("DepositToken(address account,uint256 quantity,uint256 amount,uint256 deadline,uint256 nonce,uint256 status)");
 
     address private immutable _acceptToken;
+
+    bytes32 public immutable merkleRoot;
 
     mapping(address => uint256) _accountNonces;
     uint256 private _nonce;
 
-    constructor(address acceptToken) EIP712("DepositContract", "1.0.0") {
+    constructor(address acceptToken, bytes32 merkleRoot_) EIP712("DepositContract", "1.0.0") {
         _acceptToken = acceptToken;
+        merkleRoot = merkleRoot_;
         _setupRole(DEFAULT_ADMIN_ROLE, _msgSender());
     }
 
@@ -51,6 +57,9 @@ contract DepositContract is AccessControl, EIP712 {
         return _accountNonces[account];
     }
 
+    function toBytes32(address addr) pure internal returns (bytes32) {
+        return bytes32(uint256(uint160(addr)));
+    }
     /**
     @dev Deposit Token
     @param quantity NFT items quantity
@@ -59,21 +68,39 @@ contract DepositContract is AccessControl, EIP712 {
     @param signature hashed signature
     * Contract can not execute this function
     */
-    function depositToken(uint256 quantity, uint256 amount, uint256 deadline, bytes calldata signature) external {
+    function depositToken(
+        uint256 quantity, 
+        uint256 amount, 
+        uint256 deadline, 
+        uint256 status, 
+        bytes calldata signature,
+        bytes32[] calldata merkleProof
+    ) external {
         require(_msgSender() == tx.origin, "Contract address is not allowed");
         require(block.timestamp <= deadline, "Invalid expiration in deposit");
         require(quantity <= 10, "Can not mint NFTs more than 10 NFTs");
+        require(status > 0, "Sale is not started yet");
         uint256 currentValidNonce = _accountNonces[_msgSender()];
-        require(_verify(_hash(_msgSender(), quantity, amount, deadline, currentValidNonce), signature), "Invalid signature");
+        require(_verify(_hash(_msgSender(), quantity, amount, deadline, currentValidNonce, status), signature), "Invalid signature");
+
+        // if status is private_sale
+        if (status == 1) {
+            if (merkleProof.length > 0) {
+                // Verify the merkle proof.
+                require(MerkleProof.verify(merkleProof, merkleRoot, toBytes32(msg.sender)), 'MerkleDistributor: Invalid proof.');
+            } else {
+                revert InvalidPrivateSaleAddress(_msgSender());
+            }
+        }
         unchecked {
-            ++_accountNonces[_msgSender()];
+            ++ _accountNonces[_msgSender()];
             ++ _nonce;
         }
         IERC20(_acceptToken).safeTransferFrom(_msgSender(), address(this), amount);
         emit DepositedToken(_acceptToken, _msgSender(), quantity, amount, _nonce);
     }
     
-    function _hash(address account, uint256 quantity, uint256 amount, uint256 deadline, uint256 nonce)
+    function _hash(address account, uint256 quantity, uint256 amount, uint256 deadline, uint256 nonce, uint256 status)
     internal view returns (bytes32)
     {
         return _hashTypedDataV4(keccak256(abi.encode(
@@ -82,7 +109,8 @@ contract DepositContract is AccessControl, EIP712 {
             quantity,
             amount,
             deadline,
-            nonce
+            nonce,
+            status
         )));
     }
 
