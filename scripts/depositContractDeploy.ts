5955270abdd92a40277ecefc0cc2546b41e6563e
xPainite <109281998+polarydev@users.noreply.github.com>
2022-08-10 03:40:11 -0400

    modify scripts

--- scripts/depositContractDeploy.ts
+++ scripts/depositContractDeploy.ts
@@ -5,7 +5,12 @@ import * as dotenv from "dotenv";
 dotenv.config();
 
 async function main() {
-    const tokenAddress = process.env.DEPOSIT_TOKEN_ADDRESS!;
+    // const tokenAddress = process.env.DEPOSIT_TOKEN_ADDRESS!;
+    // const tokenAddress = "0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48";  //ethereum
+    // const tokenAddress = "0x2791Bca1f2de4661ED88A30C99A7a9449Aa84174";  //polygon
+    const tokenAddress = "0xB97EF9Ef8734C71904D8002F8b6Bc66Dd9c48a6E";  //avax
+    // const tokenAddress = "0x8AC76a51cc950d9822D68b83fE1Ad97B32Cd580d";  //bsc
+    // const tokenAddress = "0x04068DA6C83AFCFA0e13ba15A6696662335D5B75";  //fantom
     const merkleRoot = process.env.MERKLE_ROOT!
     const factory = await ethers.getContractFactory("DepositContract");
     const depositContract = await factory.deploy(tokenAddress, merkleRoot);
